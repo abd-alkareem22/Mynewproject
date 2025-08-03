7c8ec16e023e32f36bd36e278d125aee16fdd11c
Abd Alkareem Tammous <abdbasem20@gmail.com>
2025-08-03 10:07:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const nav = document.querySelector('#head nav');
+    const ul = nav.querySelector('ul');
+    const register = nav.querySelector('.register');
+    
+    if (ul.style.display === 'none' || ul.style.display === '') {
+        ul.style.display = 'flex';
+        register.style.display = 'flex';
+        nav.style.height = 'auto';
+        nav.style.padding = '1rem';
+    } else {
+        ul.style.display = 'none';
+        register.style.display = 'none';
+        nav.style.height = '3rem';
+        nav.style.padding = '0.5rem';
+    }
+}
